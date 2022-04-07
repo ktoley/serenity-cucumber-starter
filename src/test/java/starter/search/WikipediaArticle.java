3ede0817664d28e0ef717e92dad20809d30ccd92
Kenneth J. Toley <kenneth.toley@ymedialabs.com>
2022-04-07 11:25:57 -0700

    fixed locator

--- src/test/java/starter/search/WikipediaArticle.java
+++ src/test/java/starter/search/WikipediaArticle.java
@@ -1,7 +1,8 @@
 package starter.search;
 
 import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
 
 public class WikipediaArticle {
-    public static final Target HEADING =  Target.the("article identifier").locatedBy("p.mw-search-createlink a");
+    public static final Target HEADING =  Target.the("article identifier").located(By.id("firstHeading"));
 }
